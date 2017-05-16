1072f338395bf19c3689d9b1aeb85c7f1fef1249
Pierre-Luc Cusson <pierre-luc.cusson.1@ens.etsmtl.ca>
2017-05-15 20:15:11 -0400

    init project

---
+++ Source/TerrainTest/TerrainTestGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TerrainTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TERRAINTEST_API ATerrainTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
